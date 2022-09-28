672f43e8a4a8d75fdeb6c8c6bd641080a855c681
Sajjad Hossain Sagor <sagorh672@gmail.com>
2022-09-29 01:39:33 +0600

    Minor Update.. tested for latest wp compatibility.

---
+++ disposable-email-blocker-gravityforms.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Plugin Name: Disposable Email Blocker - GravityForms
+ * Plugin URI: https://wordpress.org/plugins/disposable-email-blocker-wpforms/
+ * Author: Sajjad Hossain Sagor
+ * Description: Prevent From Submitting Any Disposable/Temporary Emails On GravityForms Forms.
+ * Version: 1.0.1
+ * Author URI: https://sajjadhsagor.com
+ * Text Domain: disposable-email-blocker-gravityforms
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// plugin root path....
+define( 'DEBGFFORMS_ROOT_DIR', dirname( __FILE__ ) );
+
+// plugin root url....
+define( 'DEBGFFORMS_ROOT_URL', plugin_dir_url( __FILE__ ) );
+
+// plugin version
+define( 'DEBGFFORMS_VERSION', '1.0.0' );
+
+// load translation files...
+add_action( 'plugins_loaded', 'debgfforms_load_plugin_textdomain' );
+
+function debgfforms_load_plugin_textdomain()
+{	
+	load_plugin_textdomain( 'disposable-email-blocker-gravityforms', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+add_filter( 'gform_form_settings', 'debgfforms_add_new_form_field', 10, 2 );
+
+function debgfforms_add_new_form_field( $settings, $form )
+{    
+    $settings[ __( 'Form Options', 'gravityforms' ) ]['block_disposable_emails'] = '
+        <tr>
+            <th>Block Disposable Emails</th>
+            <td><input type="checkbox" ' . checked( rgar( $form, 'block_disposable_emails' ), 'on', false ) . ' name="block_disposable_emails" id="block_disposable_emails">
+            	<label for="block_disposable_emails">Enable Blocking Disposable/Temporary Emails</label>
+            </td>
+        </tr>
+        <tr>
+            <th>Disposable Email Found Text</th>
+            <td><input type="text" class="fieldwidth-3" value="' . rgar( $form, 'disposable_emails_found_msg' ) . '" name="disposable_emails_found_msg" id="disposable_emails_found_msg" placeholder="Disposable/Temporary emails are not allowed!">
+            </td>
+        </tr>';
+ 
+    return $settings;
+}
+ 
+// save your custom form setting
+add_filter( 'gform_pre_form_settings_save', 'save_debgfforms_form_field' );
+
+function save_debgfforms_form_field( $form )
+{
+	$form['block_disposable_emails'] = rgpost( 'block_disposable_emails' );
+
+	$form['disposable_emails_found_msg'] = rgpost( 'disposable_emails_found_msg' );	
+    
+    return $form;
+}
+
+// check if disposable email is found and if so then mark form as invalid and show message
+
+add_filter( 'gform_field_validation', 'debgfforms_block_disposable_emails', 10, 4 );
+
+function debgfforms_block_disposable_emails( $result, $value, $form, $field )
+{
+	// if not blocking is enabled return early	
+	if ( $form['block_disposable_emails'] !== 'on' ) return $result;
+    
+    if ( $field->get_input_type() === 'email' )
+    {
+    	$msg = ( empty( $form['disposable_emails_found_msg'] ) ) ? 'Disposable/Temporary emails are not allowed! Please use a non temporary email' : $form['disposable_emails_found_msg'];
+
+    	if( filter_var( $value, FILTER_VALIDATE_EMAIL ) )
+    	{
+    		// split on @ and return last value of array (the domain)
+	    	$domain = explode('@', $value );
+	    	
+	    	$domain = array_pop( $domain );
+
+	    	// get domains list from json file
+			$disposable_emails_db = file_get_contents( DEBGFFORMS_ROOT_DIR . '/assets/data/domains.min.json' );
+
+			// convert json to php array
+			$disposable_emails = json_decode( $disposable_emails_db );
+
+			// check if domain is in disposable db
+			if ( in_array( $domain, $disposable_emails ) )
+			{	
+				$result['is_valid'] = false;
+
+				$result['message']  = $msg;
+			}
+	    }
+    }
+
+    return $result;
+}
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Disposable Email Blocker - GravityForms ===
+Contributors: sajjad67
+Tags: disposable-email-blocker, temporary-email-blocker, blocker, temporary-email, gravity-forms
+Requires at least: 5.6
+Tested up to: 6.0
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Prevent From Submitting Any Disposable/Temporary Emails On GravityForms Forms.
+
+== Description ==
+Using This Plugin You Can Block Spammy Disposable & Temporary Email Domains From Submit On GravityForms form. Filtering Spammy Emails Will Keep Your Subscriber List Spam Free.
+
+= Features: =
+- Designed As Plug & Play So That Anyone Can Use
+- Toggle Feature To Enable/Disable Blocking on Individual Form
+- Easily Change Disposable/Temporary Emails Found Error Message (Default Message Provided)
+
+Are You Getting Too Much Spammy Disposable or Temporary Emails Submission On Your GravityForms form which are not much valuable? You can use this plugin to block/prevent disposable emails like 10MinuteMail and GuerrillaMail from submission on any GravityForms form.
+
+Credits
+- https://github.com/andreis/disposable
+
+**Interested in contributing to Disposable Email Blocker - GravityForms?**
+Contact me at sagorh672(at)gmail(dot)com
+
+== Installation ==
+To add a WordPress Plugin using the built-in plugin installer:
+
+Go to Plugins > Add New.
+
+1. Type in the name "Disposable Email Blocker - GravityForms" in Search Plugins box
+2. Find the "Disposable Email Blocker - GravityForms" Plugin to install.
+3. Click Install Now to begin the plugin installation.
+4. The resulting installation screen will list the installation as successful or note any problems during the install.
+If successful, click Activate Plugin to activate it, or Return to Plugin Installer for further actions.
+
+To add a WordPress Plugin from github repo / plugin zip file :
+1. Go to wordpress plugin page
+2. Click Add New & Upload Plugin
+3. Drag / Click upload the plugin zip file
+4. The resulting installation screen will list the installation as successful or note any problems during the install.
+If successful, click Activate Plugin to activate it, or Return to Plugin Installer for further actions.
+
+== Frequently Asked Questions ==
+= How to use this plugin? =
+Just after installing Disposable Email Blocker - GravityForms plugin, create or edit existing contact form and according to screenshot enable filtering and edit message... then voila! yo're ready to go.
+
+== Screenshots ==
+1. Message Screen When Disposable/Temporary Email Found
+2. Message Edit Screen && Enable/Disable Filtering
+
+== Changelog ==
+= 1.0.1 =
+- Compatibility check for wp 6.0.
+= 1.0.0 =
+- Initial release.
+
+== Upgrade Notice ==
+Always try to keep your plugin update so that you can get the improved and additional features added to this plugin up to date.
