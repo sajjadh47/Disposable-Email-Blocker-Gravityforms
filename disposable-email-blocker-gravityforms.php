169a12efc7f5a1b57b11e6630d3a01aa2a4845ff
Sajjad Hossain Sagor <sagorh672@gmail.com>
2024-09-14 21:50:11 +0600

    Update disposable-email-blocker-gravityforms.php

--- disposable-email-blocker-gravityforms.php
+++ disposable-email-blocker-gravityforms.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://wordpress.org/plugins/disposable-email-blocker-wpforms/
  * Author: Sajjad Hossain Sagor
  * Description: Prevent From Submitting Any Disposable/Temporary Emails On GravityForms Forms.
- * Version: 1.0.2
+ * Version: 1.0.4
  * Author URI: https://sajjadhsagor.com
  * Text Domain: disposable-email-blocker-gravityforms
  */
@@ -18,7 +18,7 @@ define( 'DEBGFFORMS_ROOT_DIR', dirname( __FILE__ ) );
 define( 'DEBGFFORMS_ROOT_URL', plugin_dir_url( __FILE__ ) );
 
 // plugin version
-define( 'DEBGFFORMS_VERSION', '1.0.0' );
+define( 'DEBGFFORMS_VERSION', '1.0.4' );
 
 // load translation files...
 add_action( 'plugins_loaded', 'debgfforms_load_plugin_textdomain' );
@@ -32,20 +32,20 @@ add_filter( 'gform_form_settings', 'debgfforms_add_new_form_field', 10, 2 );
 
 function debgfforms_add_new_form_field( $settings, $form )
 {    
-    $settings[ __( 'Form Options', 'gravityforms' ) ]['block_disposable_emails'] = '
-        <tr>
-            <th>Block Disposable Emails</th>
-            <td><input type="checkbox" ' . checked( rgar( $form, 'block_disposable_emails' ), 'on', false ) . ' name="block_disposable_emails" id="block_disposable_emails">
-            	<label for="block_disposable_emails">Enable Blocking Disposable/Temporary Emails</label>
-            </td>
-        </tr>
-        <tr>
-            <th>Disposable Email Found Text</th>
-            <td><input type="text" class="fieldwidth-3" value="' . rgar( $form, 'disposable_emails_found_msg' ) . '" name="disposable_emails_found_msg" id="disposable_emails_found_msg" placeholder="Disposable/Temporary emails are not allowed!">
-            </td>
-        </tr>';
+	$settings[ __( 'Form Options', 'gravityforms' ) ]['block_disposable_emails'] = '
+		<tr>
+			<th>Block Disposable Emails</th>
+			<td><input type="checkbox" ' . checked( rgar( $form, 'block_disposable_emails' ), 'on', false ) . ' name="block_disposable_emails" id="block_disposable_emails">
+				<label for="block_disposable_emails">Enable Blocking Disposable/Temporary Emails</label>
+			</td>
+		</tr>
+		<tr>
+			<th>Disposable Email Found Text</th>
+			<td><input type="text" class="fieldwidth-3" value="' . rgar( $form, 'disposable_emails_found_msg' ) . '" name="disposable_emails_found_msg" id="disposable_emails_found_msg" placeholder="Disposable/Temporary emails are not allowed!">
+			</td>
+		</tr>';
  
-    return $settings;
+	return $settings;
 }
  
 // save your custom form setting
@@ -56,8 +56,8 @@ function save_debgfforms_form_field( $form )
 	$form['block_disposable_emails'] = rgpost( 'block_disposable_emails' );
 
 	$form['disposable_emails_found_msg'] = rgpost( 'disposable_emails_found_msg' );	
-    
-    return $form;
+	
+	return $form;
 }
 
 // check if disposable email is found and if so then mark form as invalid and show message
@@ -68,19 +68,19 @@ function debgfforms_block_disposable_emails( $result, $value, $form, $field )
 {
 	// if not blocking is enabled return early	
 	if ( $form['block_disposable_emails'] !== 'on' ) return $result;
-    
-    if ( $field->get_input_type() === 'email' )
-    {
-    	$msg = ( empty( $form['disposable_emails_found_msg'] ) ) ? 'Disposable/Temporary emails are not allowed! Please use a non temporary email' : $form['disposable_emails_found_msg'];
+	
+	if ( $field->get_input_type() === 'email' )
+	{
+		$msg = ( empty( $form['disposable_emails_found_msg'] ) ) ? 'Disposable/Temporary emails are not allowed! Please use a non temporary email' : $form['disposable_emails_found_msg'];
 
-    	if( filter_var( $value, FILTER_VALIDATE_EMAIL ) )
-    	{
-    		// split on @ and return last value of array (the domain)
-	    	$domain = explode('@', $value );
-	    	
-	    	$domain = array_pop( $domain );
+		if( filter_var( $value, FILTER_VALIDATE_EMAIL ) )
+		{
+			// split on @ and return last value of array (the domain)
+			$domain = explode('@', $value );
+			
+			$domain = array_pop( $domain );
 
-	    	// get domains list from json file
+			// get domains list from json file
 			$disposable_emails_db = file_get_contents( DEBGFFORMS_ROOT_DIR . '/assets/data/domains.min.json' );
 
 			// convert json to php array
@@ -93,8 +93,8 @@ function debgfforms_block_disposable_emails( $result, $value, $form, $field )
 
 				$result['message']  = $msg;
 			}
-	    }
-    }
+		}
+	}
 
-    return $result;
+	return $result;
 }
